3833aaa4f97842801a8497787c4303ecee45ab6c
alfonso porto <alfonso.porto@colex.local>
2022-07-06 18:18:49 +0200

    Add test for substraction method and array

--- test/mainTests.js
+++ test/mainTests.js
@@ -1,14 +1,39 @@
-const assert=require('chai').assert;
+const assert = require('chai').assert;
 
-const main=require('../main');
+const main = require('../main');
 
 //Results
-const helloWorlText=main.helloWorld();
-const substractionResult=main.substraction(4,2);
-const arrayOfNumbers=main.arrayOfNumbers();
+const helloWorlText = main.helloWorld();
+const substractionResult = main.substraction(4, 2);
+const arrayOfNumbers = main.arrayOfNumbers();
 
-describe('Main Suite',function(){
-    it('Hello World text is as expected',function(){
-        assert.equal(helloWorlText,'hello wrld');
+describe('Main Suite', function () {
+    describe("Hello World Method - Test Cases", function () {
+        it('Hello World text is as expected', function () {
+            assert.equal(helloWorlText, 'hello world');
+        });
+        it('Validate data type', function () {
+            assert.typeOf(helloWorlText, 'string', 'The expected data type is an string')
+        })
+        it('Lenght of string', function () {
+            assert.lengthOf(helloWorlText, 11, 'The lenght is expected to be 11')
+        })
+    });
+    describe("Substraction method - Test Cases", function () {
+        it('Less than 5', function () {
+            assert.isBelow(substractionResult, 5);
+        });
+        it('Less than 5', function () {
+            assert.isBelow(substractionResult, 5);
+        });
+        it('Validate data type', function () {
+            assert.typeOf(substractionResult, 'number', 'The expected data type is an number')
+        })
     })
+    describe("Array method - Test Cases", function () {
+        it('Array includes de number 5', function () {
+            assert.include(arrayOfNumbers, 5);
+        })
+    })
+
 })
